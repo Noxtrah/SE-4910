8d2c21a33a9dc6002d7442e13877ee70c22133e2
Eylül Özatman <eylulozatman@gmail.com>
2023-11-09 16:43:18 +0300

    token-security

--- src/main/java/se4910/recipiebeckend/config/SecurityConfig.java
+++ src/main/java/se4910/recipiebeckend/config/SecurityConfig.java
@@ -41,11 +41,7 @@ public class SecurityConfig  {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
-                .authorizeRequests()
-                .requestMatchers("/rest/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .SessionCreationPolicy.STATELESS;
+
 
         return http.build();
     }
