62cd1984da7bda95a36ff7e0a9d1e46bcad313c2
Doğa Kayra Yılmazarslan <94742646+Noxtrah@users.noreply.github.com>
2023-12-21 23:41:54 +0300

    Login username added

--- register.js
+++ register.js
@@ -32,3 +32,61 @@ backButton.addEventListener("click", function(event) {
     document.querySelector(".register-box").style.display = "none";
 
 });
+
+function registerUser() {
+    const userName = document.querySelector('#registerUserName').value;
+    const name = document.querySelector('input[type="name"]').value;
+    const lastName = document.querySelector('input[type="last-name"]').value;
+    const email = document.querySelector('#registerMail').value;
+    const password = document.querySelector('#registerPassword').value;
+    const confirmPassword = document.querySelector('input[type="password-confirm"]').value;
+    const day = document.getElementById('day').value;
+    const month = document.getElementById('month').value;
+    const year = document.getElementById('year').value;
+
+    const birthday = `${year}-${month}-${day}`;
+
+    const termsAndConditions = document.querySelector('#Terms').checked;
+    console.log(termsAndConditions)
+
+    if (!termsAndConditions) {
+        alert("Please agree to the Terms & Conditions and Privacy Policy.");
+        return;
+    }
+
+    const user = {
+        name: name,
+        lastName: lastName,
+        username: userName,
+        email: email,
+        birthday: birthday,
+        password: password
+        // add other fields as needed
+    };
+
+    // Make the POST request to your Java backend
+    fetch('http://recipiebeckend.azurewebsites.net/auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Registration failed');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // handle success, e.g., redirect to another page
+        console.log(data);
+    })
+    .catch(error => {
+        // handle error
+        console.error('Error:', error);
+    });
+}
+
+document.getElementById('register-button').addEventListener('click', registerUser);
+
