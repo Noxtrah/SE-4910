8557ad0da7b9674b41bbf2eec86ebb8812711a75
Doğa Kayra Yılmazarslan <94742646+Noxtrah@users.noreply.github.com>
2024-02-17 15:09:15 +0300

    Basich Search method added.

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,11 +1,11 @@
 
-const placeholder = '<span class="searchPlaceholder">Search...</span>'
+// const placeholder = '<span class="searchPlaceholder">Search...</span>'
 
-//The tags currently displayed
-const currentTags = ["ingredients: apple"];
+// //The tags currently displayed
+// const currentTags = ["ingredients: apple"];
 
-//Tags the user can choose from
-const tags = ["ingredients"]
+// //Tags the user can choose from
+// const tags = ["ingredients"]
 
 // window.onload = () => {
 // 	//Initialization
@@ -23,95 +23,95 @@ const tags = ["ingredients"]
 // }
 
 //The search bar needs to be scrollable, but we don't want to see that ugly scrollbar
-function getRidOfScrollbar() {
-	var inputWrapper = document.getElementById('inputWrapper');
-	inputWrapper.style.paddingBottom = inputWrapper.offsetHeight - inputWrapper.clientHeight + "px";
-}
+// function getRidOfScrollbar() {
+// 	var inputWrapper = document.getElementById('inputWrapper');
+// 	inputWrapper.style.paddingBottom = inputWrapper.offsetHeight - inputWrapper.clientHeight + "px";
+// }
 
-function handleFocus(event) {
-	let input = document.getElementById("searchInput")
-	if (input.children.length > 0) {
-		input.innerHTML = ''
-		input.removeEventListener("focus", handleFocus);
-	}
-}
+// function handleFocus(event) {
+// 	let input = document.getElementById("searchInput")
+// 	if (input.children.length > 0) {
+// 		input.innerHTML = ''
+// 		input.removeEventListener("focus", handleFocus);
+// 	}
+// }
 
-//When pressing enter, prevent it from creating a newline, finish tag instead
-function blockEnter(event) {
-	if (event.key === "Enter") {
-		event.preventDefault();
-		if (event.target.classList.contains('tagEditor')) {
-			saveTag(event);
-		} else {
-			handleChange(event);
-		}
-	}
-}
+// //When pressing enter, prevent it from creating a newline, finish tag instead
+// function blockEnter(event) {
+// 	if (event.key === "Enter") {
+// 		event.preventDefault();
+// 		if (event.target.classList.contains('tagEditor')) {
+// 			saveTag(event);
+// 		} else {
+// 			handleChange(event);
+// 		}
+// 	}
+// }
 
 //Checks if the user typed a new tag, and if so, adds it to currentTags
-function handleChange(event) {
-	let str = event.target.innerText;
-	for (let tag of tags) {
-		let regexp;
-		if (event.key === 'Enter') {
-			//When `Enter` was pressed we do not have to check for a comma or a space
-			regexp = new RegExp(`@${tag} ([^ ,\u00A0]+)`);
-		} else {
-			regexp = new RegExp(`@${tag} ([^ ,\u00A0]+)[ ,\u00A0]`);
-		}
-		if (regexp.test(str)) {
-			//User typed a new tag, add it to currentTags
-			let selected = str.match(regexp)[1];
-			currentTags.push(tag + ": " + selected)
-			event.target.innerText = event.target.innerText.replace(regexp, "");
-			updateTags()
-		}
-	}
+// function handleChange(event) {
+// 	let str = event.target.innerText;
+// 	for (let tag of tags) {
+// 		let regexp;
+// 		if (event.key === 'Enter') {
+// 			//When `Enter` was pressed we do not have to check for a comma or a space
+// 			regexp = new RegExp(`@${tag} ([^ ,\u00A0]+)`);
+// 		} else {
+// 			regexp = new RegExp(`@${tag} ([^ ,\u00A0]+)[ ,\u00A0]`);
+// 		}
+// 		if (regexp.test(str)) {
+// 			//User typed a new tag, add it to currentTags
+// 			let selected = str.match(regexp)[1];
+// 			currentTags.push(tag + ": " + selected)
+// 			event.target.innerText = event.target.innerText.replace(regexp, "");
+// 			updateTags()
+// 		}
+// 	}
 
-}
+// }
 
-//Renders the tags in currentTags in the search bar
-function updateTags() {
-	let displayStr = "";
-	for (let tag of currentTags) {
-		displayStr += `<div class='tag'><button class="tagText" onclick="editTag(event)">${tag}</button><button class="x-button" onclick="deleteTag(event)">\u2716</button></div>`
-	}
-	document.getElementById("tagWrapper").innerHTML = displayStr;
+// //Renders the tags in currentTags in the search bar
+// function updateTags() {
+// 	let displayStr = "";
+// 	for (let tag of currentTags) {
+// 		displayStr += `<div class='tag'><button class="tagText" onclick="editTag(event)">${tag}</button><button class="x-button" onclick="deleteTag(event)">\u2716</button></div>`
+// 	}
+// 	document.getElementById("tagWrapper").innerHTML = displayStr;
 
-	//Focus the input
-	input = document.getElementById('searchInput')
-	input.focus();
-	let range = document.createRange();
-	range.selectNodeContents(input);
-	range.collapse(false);
-	let selection = window.getSelection();
-	selection.removeAllRanges();
-	selection.addRange(range);
-}
+// 	//Focus the input
+// 	input = document.getElementById('searchInput')
+// 	input.focus();
+// 	let range = document.createRange();
+// 	range.selectNodeContents(input);
+// 	range.collapse(false);
+// 	let selection = window.getSelection();
+// 	selection.removeAllRanges();
+// 	selection.addRange(range);
+// }
 
 //When user clicked on a tag, make that tag editable
-function editTag(event) {
-	let text = event.target.innerText
-	let index = currentTags.indexOf(text)
-	event.target.parentElement.innerHTML = `<span>${text.substring(0,text.indexOf(':')+1)}&nbsp;</span><span onkeydown="blockEnter(event)" class="tagEditor" id="tag${index}" contenteditable>${text.substring(text.indexOf(':')+1)}</span><button class="x-button" onclick="deleteTag(event)">\u2716</button>`
-	let newInput = document.getElementById('tag' + index)
+// function editTag(event) {
+// 	let text = event.target.innerText
+// 	let index = currentTags.indexOf(text)
+// 	event.target.parentElement.innerHTML = `<span>${text.substring(0,text.indexOf(':')+1)}&nbsp;</span><span onkeydown="blockEnter(event)" class="tagEditor" id="tag${index}" contenteditable>${text.substring(text.indexOf(':')+1)}</span><button class="x-button" onclick="deleteTag(event)">\u2716</button>`
+// 	let newInput = document.getElementById('tag' + index)
 
-	newInput.focus()
-  	window.getSelection().collapse(newInput.firstChild, newInput.innerHTML.length);
-}
+// 	newInput.focus()
+//   	window.getSelection().collapse(newInput.firstChild, newInput.innerHTML.length);
+// }
 
-//User is done with editing tag, so save it and re-render the tags.
-function saveTag(event) {
-	let index = parseInt(event.target.id.substring(3),10);
-	currentTags[index] = event.target.previousSibling.innerText + event.target.innerText;
-	updateTags();
-}
+// //User is done with editing tag, so save it and re-render the tags.
+// function saveTag(event) {
+// 	let index = parseInt(event.target.id.substring(3),10);
+// 	currentTags[index] = event.target.previousSibling.innerText + event.target.innerText;
+// 	updateTags();
+// }
 
-//Deletes a tag
-function deleteTag(event) {
-	currentTags.splice(currentTags.indexOf(event.target.previousSibling.innerText),1);
-	updateTags();
-}
+// //Deletes a tag
+// function deleteTag(event) {
+// 	currentTags.splice(currentTags.indexOf(event.target.previousSibling.innerText),1);
+// 	updateTags();
+// }
 
 function toggleFavorite(element) {
     if (element.classList.contains('favorited')) {
@@ -260,42 +260,42 @@ async function getAverageRating(index) {
 }
 
   //Call jQuery before below code
-  $('.search-description').hide();
-  $('.main-btn').click(function() {
-	$('.search-description').slideToggle(300);
-  });
-  $('.search-description li').click(function() {
-	var target = $(this).html();
-	console.log("Target = " + target);
-	var toRemove = 'By ';
-	var newTarget = target.replace(toRemove, '');
-	console.log("new Taget = ", newTarget);
-	//remove spaces
-	newTarget = newTarget.replace(/\s/g, '');
-	console.log("new Taget after = ", newTarget);
-	$(".search-large").html(newTarget);
-	$('.search-description').hide();
-	$('.main-input').hide();
-	newTarget = newTarget.toLowerCase();
-	console.log("lower new Taget = ", newTarget);
-	$('.main-' + newTarget).show();
-  });
-  $('#main-submit-mobile').click(function() {
-	$('#main-submit').trigger('click');
-  });
-  $(window).resize(function() {
-	replaceMatches();
-  });
+//   $('.search-description').hide();
+//   $('.main-btn').click(function() {
+// 	$('.search-description').slideToggle(300);
+//   });
+//   $('.search-description li').click(function() {
+// 	var target = $(this).html();
+// 	console.log("Target = " + target);
+// 	var toRemove = 'By ';
+// 	var newTarget = target.replace(toRemove, '');
+// 	console.log("new Taget = ", newTarget);
+// 	//remove spaces
+// 	newTarget = newTarget.replace(/\s/g, '');
+// 	console.log("new Taget after = ", newTarget);
+// 	$(".search-large").html(newTarget);
+// 	$('.search-description').hide();
+// 	$('.main-input').hide();
+// 	newTarget = newTarget.toLowerCase();
+// 	console.log("lower new Taget = ", newTarget);
+// 	$('.main-' + newTarget).show();
+//   });
+//   $('#main-submit-mobile').click(function() {
+// 	$('#main-submit').trigger('click');
+//   });
+//   $(window).resize(function() {
+// 	replaceMatches();
+//   });
 
-  function replaceMatches() {
-	var width = $(window).width();
-	if (width < 516) {
-	  $('.main-ingredients').attr('value', 'Ingredients');
-	} else {
-	  $('.main-ingredients').attr('value', 'Search by ingredients');
-	}
-  };
-  replaceMatches();
+//   function replaceMatches() {
+// 	var width = $(window).width();
+// 	if (width < 516) {
+// 	  $('.main-ingredients').attr('value', 'Ingredients');
+// 	} else {
+// 	  $('.main-ingredients').attr('value', 'Search by ingredients');
+// 	}
+//   };
+//   replaceMatches();
 
   function clearText(thefield) {
 	if (thefield.defaultValue == thefield.value) {
@@ -607,4 +607,23 @@ function toggleAndCloseDropdown(dropdown) {
         dropdownContent.classList.add('show-dropdown');
         console.log("toggle add");
     }
+}
+
+function basicSearch() {
+    var targetWord = document.querySelector('.main-name').value;
+    //var targetWord = document.getElementById('.main-submit').value;
+    var apiUrl = 'https://recipiebeckend.azurewebsites.net/recipes/basic-search?targetWord=' + encodeURIComponent(targetWord);
+    fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(targetWord);
+        console.log(data);
+        displayDashboard(data);
+    })
+    .catch(error => console.error('Error fetching data:', error));
 }
